ede9502179430a89eb36c6a4dbb737c940e168d9
andre-pedro <andrewgoesgaming@gmail.com>
2018-10-02 12:04:50 +0100

    Added Variables and Methods

---
+++ Aula1/FNPlayer/FNPlayer/FNPlayer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fortnite
+{
+    class FNPlayer
+    {
+        public string equippedWeapon;
+        public double hitPoints;
+        public double shield;
+
+
+        public FNPlayer()
+        {
+            equippedWeapon = "picareta";
+            hitPoints = 100;
+            shield = 0;
+        }
+
+
+        public FNPlayer(string equippedWeapon, double hitPoints, double shield)
+        {
+            this.equippedWeapon = equippedWeapon;
+            this.hitPoints = hitPoints;
+            this.shield = shield;
+        }
+
+
+        public void Attack(FNPlayer enemy)
+        {
+           enemy.TakeDamage(10);
+        }
+
+        public void TakeDamage(double damage)
+        {
+            if (shield > 0)
+            {
+                shield -= damage;
+                if (shield < 0)
+                {
+                    shield = 0;
+                }
+            }
+            else
+            {
+                hitPoints -= damage;
+                if (hitPoints <= 0)
+                {
+                    //Die();
+                }
+            }
+        }
+
+    }
+}
---
+++ Aula1/FNPlayer/FNPlayer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fortnite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FNPlayer player1 = new FNPlayer();
+            FNPlayer player2 = new FNPlayer("MachineGun", 100, 100);
+
+            player1.Attack(player2);
+
+            Console.WriteLine(player1.shield);
+        }
+    }
+}
