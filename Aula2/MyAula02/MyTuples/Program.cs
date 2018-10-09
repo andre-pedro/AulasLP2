b51b56ccb72e588068e97df2e14decfc27337e6b
andre-pedro <andrewgoesgaming@gmail.com>
2018-10-09 12:52:52 +0100

    add aula2

--- Aula2/MyAula02/MyTuples/Program.cs
+++ Aula2/MyAula02/MyTuples/Program.cs
@@ -11,6 +11,31 @@ namespace MyTuples
         static void Main(string[] args)
         {
 
+            Random rnd = new Random();
+            int randomint = rnd.Next(); // creates a number between 1 and 12
+            double randomdouble = rnd.Next();   // creates a number between 1 and 6
+            bool randombool = rnd.Next(true);     // creates a number between 0 and 51
+
+
+
+
+
+            Tuple<int, double, bool> tuple =
+                new Tuple<int, double, bool>(1, 3.1415, true);
+
+            if (tuple.Item1 == 1)
+            {
+                Console.WriteLine(tuple.Item1);
+            }
+            if (tuple.Item2 == 3.1415)
+            {
+                Console.WriteLine(tuple.Item2);
+            }
+            if (tuple.Item3)
+            {
+                Console.WriteLine(tuple.Item3);
+            }
         }
     }
 }
+
